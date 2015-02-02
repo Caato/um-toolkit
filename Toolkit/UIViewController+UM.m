1c7e55b103475dcb33ddeb759d6a4d33e7ec2b1c
Christian Scheid <christian.scheid@caato.de>
2015-02-02 13:30:15 +0000

    changes

--- Toolkit/UIViewController+UM.m
+++ Toolkit/UIViewController+UM.m
@@ -10,9 +10,13 @@
 @implementation UIViewController (UM)
 
 - (UIPopoverController*)showAsPopOverFromPoint:(CGPoint)point inView:(UIView*)view permittedArrowDirections: (UIPopoverArrowDirection)directions {
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
     UINavigationController* navController = [[UINavigationController alloc] initWithRootViewController:self];
     UIPopoverController* controller = [[UIPopoverController alloc] initWithContentViewController:navController];
     [controller presentPopoverFromRect:CGRectMake(point.x, point.y, 1, 1) inView:view permittedArrowDirections:directions animated:YES];
+    navController.navigationBar.translucent = NO;
+    navController.edgesForExtendedLayout = UIRectEdgeNone;
     return controller;
 }
 
